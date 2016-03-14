c5071df78217152cc1c1ed2e044e49eec4930a0c
Yufei Li <yufeiminds@gmail.com>
2016-03-14 22:30:33 +0800

    Initialize cue framework for developing.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App'
+import FeedsView from './components/FeedsView'
+import ReadView from './components/ReadView'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(Router)
+
+var router = new Router({
+  // HTML5 History have some issue at here
+  // history: true,
+  // root: '#!',
+  // saveScrollPosition: true,
+  linkActiveClass: 'active'
 })
+
+router.map({
+  '/feeds': {
+    name: 'feeds',
+    component: FeedsView
+  },
+  '/read/:id': {
+    name: 'read',
+    component: ReadView
+  }
+})
+
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+router.redirect({
+  '*': '/'
+})
+
+router.start(App, '#app')
